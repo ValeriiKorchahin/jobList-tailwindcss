615e90ca8eff4d0d1b6199151ded6161cb295557
ValeriiKorchahin <valerakorchagin1512@gmail.com>
2022-11-16 18:38:07 +0200

    initial commit

--- src/app/services/job.service.ts
+++ src/app/services/job.service.ts
@@ -10,13 +10,13 @@ import {IJob} from "../models/IJob";
 export class JobService {
 
   httpHeaders: HttpHeaders = new HttpHeaders({
-    Authorization: environment.TOKEN
+    Authorization: 'Bearer wm3gg940gy0xek1ld98uaizhz83c6rh2sir9f9fu'
   })
 
   constructor(private httpClient:HttpClient) { }
 
   getAll():Observable<IJob[]>{
-    return this.httpClient.get<IJob[]>(environment.API, {
+    return this.httpClient.get<IJob[]>("https://api.json-generator.com/templates/ZM1r0eic3XEy/data", {
       headers: this.httpHeaders
     })
   }
